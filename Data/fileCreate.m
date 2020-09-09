f9cad50e49d6977deee281cb4773abfeb31deaac
RITAM GUHA <ritamguha16@gmail.com>
2020-09-09 20:33:05 +0530

    Add files via upload

---
+++ Data/fileCreate.m
@@ -0,0 +1,46 @@
+function[] = fileCreate(fileName,trainPercentage)
+    
+    filePath=strcat('Files/',fileName,'.xlsx');
+    content=importdata(filePath);
+    mkdir(['Train-Test Division'],fileName);
+    
+    class=content.textdata;
+    inputData=content.data;
+    
+    [rows,~]=size(inputData);
+    uniqueClass=unique(class);
+    numClass=size(uniqueClass,1);
+    disp(uniqueClass);
+    disp(numClass);
+    oneHotClass=zeros(rows,numClass);
+    classCount=zeros(numClass,1);   
+    startClass=zeros(numClass+1,1);
+    
+    for loop=1:numClass
+        index=find(strcmp(class,uniqueClass(loop,1)));
+        classCount(loop,1)=size(index,1);
+        oneHotClass(index,loop)=1;
+        startClass(loop,1)=index(1,1);        
+    end
+    
+    startClass(numClass+1,1)=rows+1; %dummy addition to bring uniformity
+    trainNo=1;
+    testNo=1;
+    
+    
+%     fprintf('rows-%d cols-%d\n',size(inputData,1),size(inputData,2));
+    for classNo=1:numClass                
+        numTrainClass=int16(trainPercentage*classCount(classNo,1));
+        numTestClass=classCount(classNo,1)-numTrainClass;                
+        data.train(trainNo:trainNo+numTrainClass-1,:)=inputData(startClass(classNo,1):startClass(classNo,1)+numTrainClass-1,:);        
+        data.trainLabel(trainNo:trainNo+numTrainClass-1,:)=oneHotClass(startClass(classNo,1):startClass(classNo,1)+numTrainClass-1,:);
+        fprintf('index_start=%d index_end=%d data_index_start=%d data_index_end=%d\n',trainNo,trainNo+numTrainClass-1,startClass(classNo,1),startClass(classNo,1)+numTrainClass-1);
+        fprintf('index_start=%d index_end=%d data_index_start=%d data_index_end=%d\n',testNo,testNo+numTestClass-1,startClass(classNo,1)+numTrainClass,startClass(classNo+1,1)-1);
+        data.test(testNo:testNo+numTestClass-1,:)=inputData(startClass(classNo,1)+numTrainClass:startClass(classNo+1,1)-1,:);
+        data.testLabel(testNo:testNo+numTestClass-1,:)=oneHotClass(startClass(classNo,1)+numTrainClass:startClass(classNo+1,1)-1,:);        
+        trainNo=trainNo+numTrainClass;
+        testNo=testNo+numTestClass;        
+    end
+%     save(strcat('Train-Test Division/',fileName,'/',fileName,'_data.mat'),'data');
+    save(strcat('Train-Test Division/',fileName,'/',fileName,'_data.mat'),'data');
+end
